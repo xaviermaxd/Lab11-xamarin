8e30f0aad2c341b78f301f03d4d35f50c3d58c4b
Alumno <Alumno@LAB1509-17>
2023-11-25 11:19:55 -0500

    Agregar archivos de proyecto.

---
+++ MVVMDemo/MVVMDemo/Views/Operaciones.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MVVMDemo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Operaciones : ContentPage
+    {
+        public Operaciones()
+        {
+            InitializeComponent();
+
+            this.BindingContext = new ViewModels.OperacionesViewModel();
+        }
+    }
+}
